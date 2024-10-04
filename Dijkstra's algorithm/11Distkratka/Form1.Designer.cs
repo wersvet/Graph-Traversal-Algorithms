a0178021317d33d63d9c3cdeb0050577a51d3be0
Alimzhan <122010975+wersvet@users.noreply.github.com>
2024-10-04 14:12:07 +0500

    Dijkstra's algorithm.

---
+++ Dijkstra's algorithm/11Distkratka/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace _11Distkratka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelboss = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(14, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(249, 795);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ОТ";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ДО";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 40;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Длина";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView2.Location = new System.Drawing.Point(284, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(333, 489);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "№ Узла";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 40;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Откуда";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 73;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Суммарный путь";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 40;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Флаг";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 40;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(845, 14);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 24);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // labelboss
+            // 
+            this.labelboss.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelboss.Location = new System.Drawing.Point(639, 12);
+            this.labelboss.Name = "labelboss";
+            this.labelboss.Size = new System.Drawing.Size(200, 26);
+            this.labelboss.TabIndex = 51;
+            this.labelboss.Text = "Выберите источник:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(641, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 26);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "Точка прибытия:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(845, 50);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 24);
+            this.textBox2.TabIndex = 52;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(645, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 54);
+            this.button1.TabIndex = 62;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Cyan;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(778, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 54);
+            this.button2.TabIndex = 63;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(284, 507);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(619, 52);
+            this.dataGridView3.TabIndex = 64;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 814);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelboss);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelboss;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ Dijkstra's algorithm/11Distkratka/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _11Distkratka
+{
+    public partial class Form1 : Form
+    {
+        List<int> OT = new List<int>() { 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 8, 8, 8, 8, 9, 10, 11, 12, 13, 13, 14};
+        List<int> DO = new List<int>() { 2, 3, 4, 4, 7, 6, 9, 6, 7, 6, 7, 8, 9, 10,11,10,11,12,13,10,13, 12, 14, 14, 15, 15};
+        List<int> DLINA = new List<int>() {5, 2, 9, 2, 6, 12, 5, 4, 3, 8, 3, 5, 3, 2, 12, 4, 15, 17, 7, 6, 10, 13, 5, 3, 9, 4 };
+        List<int> flag = new List<int>() {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        List<int> sumwalk1 = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = OT.Count;
+            dataGridView1.ColumnCount = 3;
+            for (int i = 0; i < OT.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = OT[i].ToString();
+                dataGridView1.Rows[i].Cells[1].Value = DO[i].ToString();
+                dataGridView1.Rows[i].Cells[2].Value = DLINA[i].ToString();
+            }
+            dataGridView2.RowCount = sumwalk1.Count;
+            dataGridView2.ColumnCount = 4;
+            for (int u = 0; u < sumwalk1.Count; u++)
+            {
+                dataGridView2.Rows[u].Cells[0].Value = (u + 1).ToString();
+            }
+        }
+
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nach = int.Parse(textBox1.Text);
+            dataGridView2.Rows[nach - 1].Cells[1].Value = 0.ToString();
+            flag[nach - 1] = 1;
+            dataGridView2.Rows[nach - 1].Cells[3].Value = flag[nach - 1].ToString();
+            int konetch = int.Parse(textBox2.Text);
+            List<int> vetka = new List<int>();
+            List<int> newNumbers = new List<int>() { nach };
+            int chtoto = 0;
+            //  while (chtoto != sumwalk1.Count)
+            while (flag[konetch - 1] != 1)
+            {
+                foreach (int otchego in newNumbers)
+                {
+                    for (int i = 0; i < OT.Count; i++)
+                    {
+                        if (OT[i] == otchego && flag[DO[i] - 1] != 1)
+                        {
+                            if (dataGridView2.Rows[DO[i] - 1].Cells[2].Value == null)
+                            {
+                                vetka.Add(DO[i]);
+                                sumwalk1[DO[i] - 1] = sumwalk1[otchego - 1] + DLINA[i];
+                                dataGridView2.Rows[DO[i] - 1].Cells[1].Value = otchego.ToString();
+                                dataGridView2.Rows[DO[i] - 1].Cells[2].Value = sumwalk1[DO[i] - 1].ToString();
+                            }
+                            else
+                            {
+                                int stoitdlina = Convert.ToInt32(dataGridView2.Rows[DO[i] - 1].Cells[2].Value);
+                                int novayadlina = sumwalk1[otchego - 1] + DLINA[i];
+                                if (stoitdlina >= novayadlina)
+                                {
+                                    sumwalk1[DO[i] - 1] = novayadlina;
+                                    dataGridView2.Rows[DO[i] - 1].Cells[1].Value = otchego.ToString();
+                                    dataGridView2.Rows[DO[i] - 1].Cells[2].Value = sumwalk1[DO[i] - 1].ToString();
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                        else if (DO[i] == otchego && flag[OT[i] - 1] != 1)
+                        {
+                            if (dataGridView2.Rows[OT[i] - 1].Cells[2].Value == null)
+                            {
+                                vetka.Add(OT[i]);
+                                sumwalk1[OT[i] - 1] = sumwalk1[otchego - 1] + DLINA[i];
+                                dataGridView2.Rows[OT[i] - 1].Cells[1].Value = otchego.ToString();
+                                dataGridView2.Rows[OT[i] - 1].Cells[2].Value = sumwalk1[OT[i] - 1].ToString();
+                            }
+                            else
+                            {
+                                int stoitdlina = Convert.ToInt32(dataGridView2.Rows[OT[i] - 1].Cells[2].Value);
+                                int novayadlina = sumwalk1[otchego - 1] + DLINA[i];
+                                if (stoitdlina >= novayadlina)
+                                {
+                                    sumwalk1[OT[i] - 1] = novayadlina;
+                                    dataGridView2.Rows[OT[i] - 1].Cells[1].Value = otchego.ToString();
+                                    dataGridView2.Rows[OT[i] - 1].Cells[2].Value = sumwalk1[OT[i] - 1].ToString();
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+                int min = 100;
+                int minIndex = 0;
+                for (int i = 0; i < sumwalk1.Count; i++) // 15
+                {
+                    if (sumwalk1[i] != 0 && sumwalk1[i] < min && flag[i] == 0)
+                    {
+                        min = sumwalk1[i];
+                        minIndex = i;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                flag[minIndex] = 1;
+                dataGridView2.Rows[minIndex].Cells[3].Value = flag[minIndex].ToString();
+                newNumbers.Clear();
+                newNumbers.Add(minIndex + 1);
+                vetka.Remove(minIndex + 1);
+                chtoto = 0;
+                for (int i = 0; i < 15; i++)
+                {
+                    if (flag[i] == 1)
+                    {
+                        chtoto++;
+                    }
+                }
+            }
+            // konetch
+            List<int> itogovpos = new List<int>() {};
+            int forfinish = konetch;
+            while(forfinish != 0)
+            {
+                itogovpos.Add(forfinish);
+                dataGridView2.Rows[forfinish - 1].Cells[0].Style.BackColor = Color.Green;
+                forfinish = Convert.ToInt32(dataGridView2.Rows[forfinish - 1].Cells[1].Value);
+            }
+            int wopi = 0;
+            dataGridView3.RowCount = 1;
+            dataGridView3.ColumnCount = itogovpos.Count;
+            itogovpos.Reverse();
+            foreach (int i in itogovpos)
+            {
+                dataGridView3.Rows[0].Cells[wopi].Value = i.ToString();
+                dataGridView3.Columns[wopi].Width = 50;
+                wopi++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text = string.Empty;
+            dataGridView2.RowCount = sumwalk1.Count;
+            dataGridView2.ColumnCount = 4;
+            for (int u = 0; u <sumwalk1.Count; u++)
+            {
+                dataGridView2.Rows[u].Cells[0].Value = (u + 1).ToString();
+                dataGridView2.Rows[u].Cells[1].Value = "".ToString();
+                dataGridView2.Rows[u].Cells[2].Value = "".ToString();
+                dataGridView2.Rows[u].Cells[3].Value = "".ToString();
+                dataGridView2.Rows[u].Cells[0].Style.BackColor = Color.White;
+            }
+            flag.Clear();
+            sumwalk1.Clear();
+           // vetka.Clear();
+            for (int u = 0; u <sumwalk1.Count; u++)
+            {
+                flag.Add(0);
+                sumwalk1.Add(0);
+            }
+        }
+    }
+}
