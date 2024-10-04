47850f3ddf943f1580079efbcceab9d451361b62
Alimzhan <122010975+wersvet@users.noreply.github.com>
2024-10-04 14:14:45 +0500

    Kruskal's algorithm

---
+++ Kruskal's algorithm/14weekdiskretka/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace _14weekdiskretka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView2.Location = new System.Drawing.Point(279, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(339, 838);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ОТ";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "ДО";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Длина";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 50;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Метка";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 50;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView3.Location = new System.Drawing.Point(644, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(339, 838);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "ОТ";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "ДО";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Длина";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Метка";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 50;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(249, 639);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Узел";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 60;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "N дерева";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 65;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1002, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 54);
+            this.button1.TabIndex = 69;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1002, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 54);
+            this.button2.TabIndex = 70;
+            this.button2.Text = "Сортировка";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 984);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Kruskal's algorithm/14weekdiskretka/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _14weekdiskretka
+{
+    public partial class Form1 : Form
+    {
+        List<int> OT = new List<int>() { 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4, 5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 10, 10, 10, 11, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 16, 17, 18, 19 };
+        List<int> DO = new List<int>() { 2, 3, 4, 5, 3, 6, 7, 4, 7, 5, 9, 10, 11, 11, 7, 12, 8, 9, 12, 9, 12, 13, 14, 10, 14, 11, 15, 16, 16, 13, 17, 17, 18, 14, 15, 18, 20, 19, 20, 19, 18, 20, 20 };
+        List<int> DLINA = new List<int>() { 6, 3, 13, 5, 12, 10, 6, 20, 4, 8, 11, 4, 8, 2, 9, 15, 5, 14, 7, 8, 18, 12, 5, 1, 16, 10, 19, 3, 5, 21, 13, 6, 1, 9, 3, 6, 24, 17, 9, 9, 7, 12, 17 };
+        List<int> nderevo = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        List<int> flagduga = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        List<int> N = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        int trid = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = N.Count;
+            for (int i = 0; i < N.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = N[i].ToString();
+            }
+            dataGridView2.RowCount = OT.Count;
+
+            for (int i = 0; i < OT.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = OT[i].ToString();
+                dataGridView2.Rows[i].Cells[1].Value = DO[i].ToString();
+                dataGridView2.Rows[i].Cells[2].Value = DLINA[i].ToString();
+                dataGridView2.Rows[i].Cells[3].Value = 0.ToString();
+                dataGridView2.Rows[i].Height = 15;
+            }
+            dataGridView3.RowCount = OT.Count;
+            for (int i = 0; i < OT.Count; i++)
+            {
+                dataGridView3.Rows[i].Height = 30;
+            }
+        }
+        int nomerderevo = 1;
+        int wopi = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int minDLINA = DLINA.Min();
+            int OTmin = 0;
+            int DOmin = 0;
+            if (flagduga[DLINA.IndexOf(minDLINA)] == 0)
+            {
+                OTmin = OT[DLINA.IndexOf(minDLINA)];
+                DOmin = DO[DLINA.IndexOf(minDLINA)];
+                if (nderevo[OTmin - 1] == 0 && nderevo[DOmin - 1] == 0)
+                {
+                    nderevo[OTmin - 1] = nomerderevo;
+                    nderevo[DOmin - 1] = nomerderevo;
+                    nomerderevo++;
+
+                }
+                else if (nderevo[OTmin - 1] != 0 && nderevo[DOmin - 1] == 0)
+                {
+                    nderevo[DOmin - 1] = nderevo[OTmin - 1];
+                }
+                else if (nderevo[OTmin - 1] == 0 && nderevo[DOmin - 1] != 0)
+                {
+                    nderevo[OTmin - 1] = nderevo[DOmin - 1];
+                }
+                else if (nderevo[OTmin - 1] != 0 && nderevo[DOmin - 1] != 0)
+                {
+                    if (nderevo[OTmin - 1] < nderevo[DOmin - 1])
+                    {
+                        for (int i = 0; i < nderevo.Count; i++)
+                        {
+                            if (nderevo[i] == nderevo[DOmin - 1])
+                            {
+                                nderevo[i] = nderevo[OTmin - 1];
+                            }
+                        }
+                    }
+                    else if (nderevo[DOmin - 1] < nderevo[OTmin - 1])
+                    {
+                        for (int i = 0; i < nderevo.Count; i++)
+                        {
+                            if (nderevo[i] == nderevo[OTmin - 1])
+                            {
+                                nderevo[i] = nderevo[DOmin - 1];
+                            }
+                        }
+                    }
+                }
+                else if (nderevo[OTmin - 1] == nderevo[DOmin - 1])
+                {
+                    
+                }
+                dataGridView3.Rows[wopi].Cells[2].Value = minDLINA.ToString();
+                dataGridView3.Rows[wopi].Cells[0].Value = OTmin.ToString();
+                dataGridView3.Rows[wopi].Cells[1].Value = DOmin.ToString();
+                flagduga[DLINA.IndexOf(minDLINA)] = 2;
+                dataGridView3.Rows[wopi].Cells[3].Value = flagduga[DLINA.IndexOf(minDLINA)].ToString();
+
+                dataGridView1.Rows[OTmin - 1].Cells[1].Value = nderevo[OTmin - 1].ToString();
+                dataGridView1.Rows[DOmin - 1].Cells[1].Value = nderevo[DOmin - 1].ToString();
+
+                dataGridView2.Rows[DLINA.IndexOf(minDLINA)].Cells[3].Value = flagduga[DLINA.IndexOf(minDLINA)].ToString();
+
+                DLINA[DLINA.IndexOf(minDLINA)] = 100;
+                wopi++;
+                trid++;
+            }
+
+            if (trid == 31)
+            {
+                dataGridView1.Rows[18 - 1].Cells[1].Value = 1.ToString();
+            }
+
+
+
+
+            int provkon = 0;
+            for (int u = 0; u<20; u++)
+            {
+                if (nderevo[u] == 1)
+                {
+                    provkon++;
+                }
+            }
+            if (provkon == 20)
+            {
+                MessageBox.Show("ВСЕ");
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+        public void Sorting()
+        {
+            int n = DLINA.Count;
+            for (int i = 1; i < n; ++i)
+            {
+                int keyDLINA = DLINA[i];
+                int keyOT = OT[i];
+                int keyDO = DO[i];
+                int j = i - 1;
+
+                while (j >= 0 && DLINA[j] > keyDLINA)
+                {
+                    DLINA[j + 1] = DLINA[j];
+                    OT[j + 1] = OT[j];
+                    DO[j + 1] = DO[j];
+                    j = j - 1;
+                }
+                DLINA[j + 1] = keyDLINA;
+                OT[j + 1] = keyOT;
+                DO[j + 1] = keyDO;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sorting();
+            for (int i = 0; i < OT.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = OT[i].ToString();
+                dataGridView2.Rows[i].Cells[1].Value = DO[i].ToString();
+                dataGridView2.Rows[i].Cells[2].Value = DLINA[i].ToString();
+                dataGridView2.Rows[i].Cells[3].Value = 0.ToString();
+                dataGridView2.Rows[i].Height = 15;
+            }
+        }
+    }
+}
